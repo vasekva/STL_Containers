d45e257497dd5f185712104b6317661605e82217
vasekva <vahonin_vasek@mail.ru>
2022-03-08 23:04:34 +0300

    Temporary commit with error with erase() test

--- tests/t_set.cpp
+++ tests/t_set.cpp
@@ -14,7 +14,7 @@ void test_set()
 	typedef allocator<char> Myal;
 	typedef less<char> Mypred;
 	typedef ft::set<char, Mypred, Myal> Mycont;
-	char ch, str1[] = "abc", carr2[] = "def";
+	char ch, str1[] = "abc", str2[] = "def";
 
 	Mycont::key_type        *p_key = (char *)0;
 	Mycont::key_compare     *p_komp = (Mypred *)0;
@@ -39,29 +39,45 @@ void test_set()
 
 	Mycont v1(str1, str1 + 3);
 	assert(v1.size() == 3 && *v1.begin() == 'a');
-
 	Mycont v2(str1, str1 + 3, pred);
 	assert(v2.size() == 3 && *v2.begin() == 'a');
-
 	Mycont v3(str1, str1 + 3, pred, al);
 	assert(v3.size() == 3 && *v3.begin() == 'a');
-//
 	const Mycont v4(str1, str1 + 3);
 	v0 = v4;
 	assert(v0.size() == 3 && *v0.begin() == 'a');
 
-//	Mycont::iterator p_it(v1.begin());
-//	Mycont::const_iterator p_cit(v4.begin());
-//	Mycont::reverse_iterator p_rit(v1.begin());
-//	Mycont::const_reverse_iterator p_crit(v4.begin());
+	Mycont::iterator p_it(v1.begin());
+	Mycont::const_iterator p_cit(v4.begin());
+	Mycont::reverse_iterator p_rit(v1.rbegin());
+	Mycont::const_reverse_iterator p_crit(v4.rbegin());
+	assert(*p_it == 'a' && *--(p_it = v1.end()) == 'c');
+	assert(*p_cit == 'a' && *--(p_cit = v4.end()) == 'c');
+	assert(*p_rit == 'c' && *--(p_rit = v1.rend()) == 'a');
+	assert(*p_crit == 'c' && *--(p_crit = v1.rend()) == 'a');
+
+	v0.clear();
+	ft::pair<Mycont::iterator, bool> pib = v0.insert('d');
+	cout << *v0.begin() << endl;
+	assert(*pib.first == 'd' && pib.second);
+	assert(*--v0.end() == 'd');
+	pib = v0.insert('d');
+	assert(*pib.first == 'd' && !pib.second);
+	assert(*v0.insert(v0.begin(), 'e') == 'e');
+	v0.insert(str1, str1 + 3);
+	assert(v0.size() == 5 && *v0.begin() == 'a');
+	v0.insert(str2, str2 + 3);
+	assert(v0.size() == 6 && *--v0.end() == 'f');
+	cout << *--v0.end() << endl;
+	cout << *--v0.begin() << endl;
+
+
+	assert(*v0.erase(v0.begin()) == 'b' && v0.size() == 5);
+
+//	assert(*v0.erase(v0.begin(), ++v0.begin()) == 'c' && v0.size() == 4);
+//	assert(v0.erase('x') == 0 && v0.erase('e') == 1);
 //
-//	assert(*p_it == 'a' && *--(p_it = v1.end()) == 'c');
-//	assert(*p_cit == 'a' && *--(p_cit = v4.end()) == 'c');
-//	assert(*p_rit == 'c' && *--(p_rit = v1.rend()) == 'a');
-//	assert(*p_crit == 'c' && *--(p_crit = v1.rend()) == 'a');
-
-
-
+//	v0.clear();
 
 	cout << GREEN"SUCCESS" << NORM << endl;
 
